631fc3298134bfefb9eb0aec0f5df316b84dc149
Thibault Latxague <tlatxague@iutbayonne.univ-pau.fr>
2025-04-06 18:48:53 +0200

    Ajout cors (pour de bon)

---
+++ LaravelBDE/routes/api.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+use App\Http\Controllers\SoireesController;
+use App\Http\Controllers\ReservationController;
+use App\Http\Controllers\GoodiesController;
+
+Route::apiResource('soirees', SoireesController::class);
+Route::apiResource('reservations', ReservationController::class);
+Route::apiResource('goodies', GoodiesController::class);
