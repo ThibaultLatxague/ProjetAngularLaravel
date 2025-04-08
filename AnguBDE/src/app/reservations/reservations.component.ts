98e0dfa4fb3bf51e11abc840803f5e7ca7c19bc6
Thibault Latxague <tlatxague@iutbayonne.univ-pau.fr>
2025-04-08 11:44:24 +0200

    Ajout menu déroulant des soirees

--- AnguBDE/src/app/reservations/reservations.component.ts
+++ AnguBDE/src/app/reservations/reservations.component.ts
@@ -1,13 +1,16 @@
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgModule } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ReservationsService } from '../services/reservations.service';
+import { SoireesService } from '../services/soirees.service';
 import { Router } from '@angular/router';
 import { Reservation } from '../models/reservation.model';
+import { Soiree } from '../models/soiree.model';
 import { MatSelectModule } from '@angular/material/select';
-import { MatOptionModule } from '@angular/material/core'; // mat-option est ici
+import { MatOptionModule } from '@angular/material/core';
+import { CommonModule } from '@angular/common';
 
 import {
   FormsModule,
@@ -18,25 +21,30 @@ import {
   selector: 'app-reservations',
   templateUrl: './reservations.component.html',
   styleUrl: './reservations.component.scss',
-  imports: [FormsModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule, MatFormFieldModule, MatOptionModule, MatSelectModule],
+  imports: [FormsModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule, MatFormFieldModule, MatOptionModule, MatSelectModule, CommonModule],
 })
 export class ReservationsComponent {
   formulaire!: FormGroup;
   currentReservation!: Reservation;
   newReservation!: Reservation;
+  listSoirees!: Soiree[];
 
-  constructor(private formBuilder: FormBuilder, private reservationService: ReservationsService, private router: Router) {  }
+  constructor(private formBuilder: FormBuilder, private reservationService: ReservationsService, private soireeService: SoireesService, private router: Router) {  }
 
   ngOnInit(): void {
+    this.soireeService.getSoirees().subscribe((soirees: Soiree[]) => {
+      this.listSoirees = soirees;
+      console.log(this.listSoirees);
+    });
+
     this.formulaire = this.formBuilder.group({
       nom: [null, [Validators.required, Validators.minLength(2)]],
       email_etudiant: [null, [Validators.required, Validators.email]],
       telephone_etudiant: [null, [Validators.required, Validators.pattern(/^0[6-7]\d{8}$/)]],
-      nom_soiree: [null, [Validators.required, Validators.minLength(2)]],
+      soiree: [null, Validators.required],
       date_reservation: [null, [Validators.required]],
       statut: [null, [Validators.required]],
-      goodies: [null, [Validators.required]],
-      coutUnitaire: [null, [Validators.required, Validators.min(0)]]
+      goodies: [null, [Validators.required]]
     });    
 
     this.formulaire.valueChanges.subscribe((formValue) => {
@@ -45,14 +53,14 @@ export class ReservationsComponent {
         nomEtudiant: formValue.nom,
         email_etudiant : formValue.email_etudiant,
         telephone_etudiant: formValue.telephone_etudiant,
-        nom_soiree: formValue.nom_soiree,
+        nom_soiree: formValue.soiree.nom,
         date_reservation: formValue.date_reservation,
         statut: formValue.statut,
         goodies: formValue.goodies,
-        idSoiree: formValue.coutUnitaire
+        idSoiree: formValue.soiree.id
       };
     });
-  }
+  };
 
   
   addReservation(): void {
@@ -61,11 +69,11 @@ export class ReservationsComponent {
       nomEtudiant: this.formulaire.get('nom')?.value,
       email_etudiant: this.formulaire.get('email_etudiant')?.value,
       telephone_etudiant: this.formulaire.get('telephone_etudiant')?.value,
-      nom_soiree: this.formulaire.get('nom_soiree')?.value,
+      nom_soiree: this.formulaire.get('soiree')?.value.nom,
       date_reservation: this.formulaire.get('date_reservation')?.value,
       statut: this.formulaire.get('statut')?.value,
       goodies: this.formulaire.get('goodies')?.value,
-      idSoiree: this.formulaire.get('coutUnitaire')?.value
+      idSoiree: this.formulaire.get('soiree')?.value.id
     };    
 
     this.reservationService.addReservation(this.newReservation).subscribe({
